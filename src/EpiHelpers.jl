8a1ae63a20696b4baed3d416b0348ff9c2a78d0d
Pratik Gupte <pratikgupte16@gmail.com>
2026-07-27 14:56:01 +0100

    Fix reshaping

--- src/EpiHelpers.jl
+++ src/EpiHelpers.jl
@@ -449,9 +449,8 @@ function calc_consumption_avail(df::DataFrame,
 
     # only cumulative deaths needed
     cumulative_deaths = count_epi_affected(df, comp_deaths_vec; col_sector = col_sector)
-    cumulative_deaths = reshape(
-        sum(cumulative_deaths, dims = 2), length(cumulative_deaths)
-    )
+    cumulative_deaths = sum(cumulative_deaths, dims = 2)
+    cumulative_deaths = reshape(cumulative_deaths, length(cumulative_deaths))
 
     new_deaths = [cumulative_deaths[begin]; diff(cumulative_deaths)]
 
